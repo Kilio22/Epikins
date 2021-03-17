0e23c0d644907ba12517b9c4c82f92e5e691fae5
Kylian Balan <46964203+Kilio22@users.noreply.github.com>
2021-03-17 13:59:57 +0100

    Update init_mongo.js

--- init_mongo.js
+++ init_mongo.js
@@ -1,2 +1,6 @@
-db.users.insertOne({"email": "", "roles": ["credentials", "log", "module", "projects", "users"], "jenkinsLogin": ""});
+// Exemple:
+// db.users.insertOne({"email": "foo.bar@epitech.eu", "roles": ["credentials", "log", "module", "projects", "users"], "jenkinsLogin": "foobar"});
+// db.credentials.insertOne({"login": "foobar", "apiKey": "baz"});
+
+db.users.insertOne({"email": "firstname.lastname@epitech.eu", "roles": ["credentials", "log", "module", "projects", "users"], "jenkinsLogin": ""}); 
 db.credentials.insertOne({"login": "", "apiKey": ""});
